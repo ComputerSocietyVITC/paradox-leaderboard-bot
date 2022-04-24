bfade4f851315656771cfaad5596f7e20e0973aa
porridgewithraisins <santhanambr2002@gmail.com>
2022-04-24 12:34:31 +0530

    leaderboard bot

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import axios from "axios";
+import { Client, Intents, TextChannel } from "discord.js";
+
+const intents = [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES];
+
+const client = new Client({ intents });
+
+(async () => {
+    await client.login(process.env.TOKEN).catch(console.error);
+
+    client.once("ready", () => console.log("ready"));
+
+    const channel = client.channels.cache.get(process.env.WRITING_CHANNEL_ID!) as TextChannel;
+
+    while (1) {
+        const { data } = await axios.get(process.env.leaderboardURI!);
+        channel.sendTyping();
+        const topThree = data.slice(3) as { username: string }[];
+        const adapted = topThree.map(({ username }, idx) => `${idx + 1}: ${username}`);
+        const stringList = adapted.join("\n");
+        await channel.send(stringList);
+        await new Promise(r => setTimeout(r, Number(process.env.REFRESH_TIME)));
+    }
+})();
