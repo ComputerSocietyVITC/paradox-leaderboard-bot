a35b1ff3153648efe6533d530f089eaca8199f18
porridgewithraisins <santhanambr2002@gmail.com>
2022-04-24 13:57:14 +0530

    change fnality

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,35 @@
 import axios from "axios";
-import { Client, Intents, TextChannel } from "discord.js";
+import { Client, Intents } from "discord.js";
 
 const intents = [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES];
 
 const client = new Client({ intents });
+
+let sleeping = false;
 
 (async () => {
     await client.login(process.env.TOKEN).catch(console.error);
 
     client.once("ready", () => console.log("ready"));
 
-    const channel = client.channels.cache.get(process.env.WRITING_CHANNEL_ID!) as TextChannel;
-
-    while (1) {
-        const { data } = await axios.get(process.env.LEADERBOARD_URI!);
-        channel.sendTyping();
-        const top = data.slice(3, 6) as { username: string }[];
-        const adapted = top.map(({ username }, idx) => `${idx + 1}: ${username}`);
-        const stringList = adapted.join("\n");
-        await channel.send(stringList);
-        await new Promise(r => setTimeout(r, Number(process.env.REFRESH_TIME)));
-    }
+    client.on("messageCreate", async message => {
+        if (message.content === "#ping") {
+            message.channel.send("pong");
+            return;
+        }
+        if (message.content === "#lb") {
+            if (sleeping) {
+                await message.reply("Leaderboard was just requested");
+                return;
+            }
+            sleeping = true;
+            const { data } = await axios.get(process.env.LEADERBOARD_URI!);
+            message.channel.sendTyping();
+            const top = data.slice(3, 13) as { username: string }[];
+            const adapted = top.map(({ username }, idx) => `${idx + 1}: ${username}`);
+            const stringList = adapted.join("\n");
+            await message.channel.send(stringList);
+            new Promise(resolve => setTimeout(resolve, 20_000)).then(() => (sleeping = false));
+        }
+    });
 })();
