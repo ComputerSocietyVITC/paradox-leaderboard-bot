a16fcc57f4405e392c907718454557d87a5bf333
porridgewithraisins <santhanambr2002@gmail.com>
2022-04-24 12:40:16 +0530

    finish up

---
+++ README.md
@@ -0,0 +1,11 @@
+# paradox leaderboard bot
+
+1. hits the azure server every x(you define it) minutes so azure doesn't decommission resources.
+2. shows leaderboard in a channel in discord server (you define it)
+
+
+**Environment variables**
+
+TOKEN=the_discord_bot_token
+WRITING_CHANNEL_ID=the channel id we're writing it to
+LEADERBOARD_URI=the uri from which we are getting the leaderboard
--- src/index.ts
+++ src/index.ts
@@ -13,10 +13,10 @@ const client = new Client({ intents });
     const channel = client.channels.cache.get(process.env.WRITING_CHANNEL_ID!) as TextChannel;
 
     while (1) {
-        const { data } = await axios.get(process.env.leaderboardURI!);
+        const { data } = await axios.get(process.env.LEADERBOARD_URI!);
         channel.sendTyping();
-        const topThree = data.slice(3) as { username: string }[];
-        const adapted = topThree.map(({ username }, idx) => `${idx + 1}: ${username}`);
+        const top = data.slice(3, 6) as { username: string }[];
+        const adapted = top.map(({ username }, idx) => `${idx + 1}: ${username}`);
         const stringList = adapted.join("\n");
         await channel.send(stringList);
         await new Promise(r => setTimeout(r, Number(process.env.REFRESH_TIME)));
